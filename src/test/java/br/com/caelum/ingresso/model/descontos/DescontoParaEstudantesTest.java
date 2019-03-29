7dcc1809f55c7c0184910dc891bd49454f212a35
Thiago <thpandrade@gmail.com>
2019-03-29 12:49:54 -0300

    3.6 Exercício Opcional - Testando os demais Descontos

---
+++ src/test/java/br/com/caelum/ingresso/model/descontos/DescontoParaEstudantesTest.java
@@ -0,0 +1,29 @@
+package br.com.caelum.ingresso.model.descontos;
+
+import br.com.caelum.ingresso.model.Filme;
+import br.com.caelum.ingresso.model.Ingresso;
+import br.com.caelum.ingresso.model.Sala;
+import br.com.caelum.ingresso.model.Sessao;
+import org.junit.Assert;
+import org.junit.Test;
+
+import java.math.BigDecimal;
+import java.time.Duration;
+import java.time.LocalTime;
+
+public class DescontoParaEstudantesTest {
+
+    @Test
+    public void deveConcederDescontoDe50PorcentoParaIngressoDeEstudante() {
+
+        Sala sala = new Sala("Eldorado - IMAX", new BigDecimal("20.5"));
+        Filme filme = new Filme("Rogue One", Duration.ofMinutes(120), "SCI-FI", new BigDecimal("12"));
+        Sessao sessao = new Sessao(LocalTime.parse("10:00:00"), sala, filme);
+        Ingresso ingresso = new Ingresso(sessao, new DescontoParaEstudantes());
+
+        BigDecimal precoEsperado = new BigDecimal("16.25");
+
+        Assert.assertEquals(precoEsperado, ingresso.getPreco());
+
+    }
+}
